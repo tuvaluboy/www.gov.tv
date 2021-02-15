a395486346a4f29c871efa7cfc1fe751b25d31ba
tuvaluboy <fini.iuni@gmail.com>
2021-02-15 15:42:13 +1200

    Media frontend updated complete

--- resources/views/front/media.blade.php
+++ resources/views/front/media.blade.php
@@ -64,7 +64,7 @@
             <div class="item features-image сol-12 col-md-6 col-lg-4">
                 <div class="item-wrapper">
                     <div class="item-img">
-                    <img src="">
+                    <img src="{{asset($services->image->getUrl())}}" alt="No Image">
                     </div>
                     <div class="item-content">
                         <h5 class="item-title mbr-fonts-style display-5"><a href="{{route('media.list', $services->id)}}" class="text-primary">{{$services->title}}</a></h5>
