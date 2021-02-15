2a46d33a15069903db6aca9e3391d09f3855d02e
tuvaluboy <fini.iuni@gmail.com>
2021-02-15 15:23:46 +1200

    Media updated backend almost complete

--- resources/views/front/media.blade.php
+++ resources/views/front/media.blade.php
@@ -20,19 +20,19 @@
 
 <hr>
     <div class="align-left container">
-        
+
                 <div class="col-lg-10">
                     <p class="mbr-text mbr-fonts-style display-text"><a  href="{{route('home')}}" class="text-primary">Home</a> &gt; {{$titlename}}</p>
                 </div>
-       
+
     </div>
-<hr>  
+<hr>
 
 
 
 
 <section class="features6 cid-s9uzryvHBf mbr-parallax-background" id="features12-4c">
-        @foreach($categories->chunk(3) as $category)
+        <!-- @foreach($categories->chunk(3) as $category)
 
 
         {{-- @for($j =  0; $j < $counts ; $j++) --}}
@@ -55,64 +55,34 @@
             </div>
         </div>
 
-        @endforeach
+        @endforeach -->
 
     <div class="container">
+    @foreach($categories->chunk(3) as $category)
         <div class="row mt-4 cid-saBl8LyfSn">
+        @foreach($category as $services)
             <div class="item features-image сol-12 col-md-6 col-lg-4">
                 <div class="item-wrapper">
                     <div class="item-img">
-                        <img src="assets/images/social2-674x449.jpg" alt="" title="">
+                    <img src="">
                     </div>
                     <div class="item-content">
-                        <h5 class="item-title mbr-fonts-style display-5"><a href="news.html" class="text-primary">Latest News</a></h5>
+                        <h5 class="item-title mbr-fonts-style display-5"><a href="{{route('media.list', $services->id)}}" class="text-primary">{{$services->title}}</a></h5>
                         <h6 class="item-subtitle mbr-fonts-style mt-1 display-7">
-                            <strong>John Smith</strong><em> 10-10-2025</em></h6>
-                        <p class="mbr-text mbr-fonts-style mt-3 display-7">Mobirise Page Maker is a free and simple
-                            website builder - just drop site blocks to your page, add content and style it!</p>
+                            <!-- <strong>John Smith</strong><em> 10-10-2025</em></h6> -->
+                        <p class="mbr-text mbr-fonts-style mt-3 display-7">{!!$services->description!!}</p>
                     </div>
-                    <div class="mbr-section-btn item-footer mt-2"><a href="news.html" class="btn item-btn btn-primary display-7" target="_blank">View More
+                    <div class="mbr-section-btn item-footer mt-2"><a href="{{route('media.list', $services->id)}}" class="btn item-btn btn-primary display-7">View More
                             </a></div>
                 </div>
             </div>
-            <div class="item features-image сol-12 col-md-6 col-lg-4">
-                <div class="item-wrapper">
-                    <div class="item-img">
-                        <img src="assets/images/climate17-674x449.jpg" alt="" title="">
-                    </div>
-                    <div class="item-content">
-                        <h5 class="item-title mbr-fonts-style display-5"><a href="news.html" class="text-primary">Announcements</a></h5>
-                        <h6 class="item-subtitle mbr-fonts-style mt-1 display-7"><strong>Ann
-                                Brown</strong><em>&nbsp;09-09-2025</em></h6>
-                        <p class="mbr-text mbr-fonts-style mt-3 display-7">Mobirise Site Creator offers a huge
-                            collection of 3500+ site blocks, templates and themes with thousands flexible options.<br>
-                        </p>
-                    </div>
-                    <div class="mbr-section-btn item-footer mt-2"><a href="news.html" class="btn btn-primary item-btn display-7" target="_blank">Veiw More
-                            </a></div>
-                </div>
-            </div>
-            <div class="item features-image сol-12 col-md-6 col-lg-4">
-                <div class="item-wrapper">
-                    <div class="item-img">
-                        <img src="assets/images/school1-674x449.jpg" alt="" title="">
-                    </div>
-                    <div class="item-content">
-                        <h5 class="item-title mbr-fonts-style display-5"><a href="news.html" class="text-primary">Vacancies</a></h5>
-                        <h6 class="item-subtitle mbr-fonts-style mt-1 display-7"><strong>Jessica Doe
-                            </strong><em>08-08-2025</em></h6>
-                        <p class="mbr-text mbr-fonts-style mt-3 display-7">Mobirise Page Maker is a free and simple
-                            website builder - just drop site blocks to your page, add content and style it!</p>
-                    </div>
-                    <div class="mbr-section-btn item-footer mt-2"><a href="news.html" class="btn btn-primary item-btn display-7" target="_blank">View More
-                            </a></div>
-                </div>
-            </div>
-            
+        @endforeach
+
 
         </div>
+    @endforeach
     </div>
-    
+
 </section>
 
 <br>
