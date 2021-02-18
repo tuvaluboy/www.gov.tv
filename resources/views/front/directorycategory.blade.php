b469b77919d402593a3885aaf6b9cbd536dac3f4
Tue6194 <suijnrgerina@gmail.com>
2021-02-18 22:48:21 +1200

    Changes to dir

--- resources/views/front/directorycategory.blade.php
+++ resources/views/front/directorycategory.blade.php
@@ -47,7 +47,9 @@
                 @foreach($category as $services)
 
                     <div class="card p-3 col-12 col-md-6 col-lg-4">
-
+                        <div class="card-img pb-3">
+                            <a href="budget.html"><span class="mbr-iconfont mbrib-cash"></span></a>
+                        </div>
                         <div class="card-box">
                                     <a href="{{route('directory.list', $services->id)}}"><h4 class="card-title py-3 mbr-fonts-style display-7">{{$services->title}}</h4>
                                     <p class="mbr-text mbr-fonts-style display-4">{!!$services->description!!}</p></a>
@@ -59,7 +61,13 @@
         </div>
 
         @endforeach
+    
     </section>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 
 
 
