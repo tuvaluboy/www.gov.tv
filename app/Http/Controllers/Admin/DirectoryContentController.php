2e4fa44106c25cbcec92d88ee8c79eee8b29599c
Fini <fini.iuni@gmail.com>
2021-04-25 20:44:39 +1200

    back end changes

--- app/Http/Controllers/Admin/DirectoryContentController.php
+++ app/Http/Controllers/Admin/DirectoryContentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\DirectoryContent;
+use App\DirectorySubCategory;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Traits\MediaUploadingTrait;
 use App\Http\Requests\MassDestroyDirectoryContentRequest;
@@ -23,13 +24,15 @@ class DirectoryContentController extends Controller
     {
         abort_if(Gate::denies('directory_content_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $directoryContents = DirectoryContent::with(['ministry', 'tags', 'media'])->get();
+        $directoryContents = DirectoryContent::with(['ministry', 'tags', 'subcategories', 'media'])->get();
 
         $ministry_contents = MinistryContent::get();
 
         $tags = Tag::get();
 
-        return view('admin.directoryContents.index', compact('directoryContents', 'ministry_contents', 'tags'));
+        $directory_sub_categories = DirectorySubCategory::get();
+
+        return view('admin.directoryContents.index', compact('directoryContents', 'ministry_contents', 'tags', 'directory_sub_categories'));
     }
 
     public function create()
@@ -40,13 +43,16 @@ class DirectoryContentController extends Controller
 
         $tags = Tag::all()->pluck('name', 'id');
 
-        return view('admin.directoryContents.create', compact('ministries', 'tags'));
+        $subcategories = DirectorySubCategory::all()->pluck('title', 'id');
+
+        return view('admin.directoryContents.create', compact('ministries', 'tags', 'subcategories'));
     }
 
     public function store(StoreDirectoryContentRequest $request)
     {
         $directoryContent = DirectoryContent::create($request->all());
         $directoryContent->tags()->sync($request->input('tags', []));
+        $directoryContent->subcategories()->sync($request->input('subcategories', []));
         foreach ($request->input('files', []) as $file) {
             $directoryContent->addMedia(storage_path('tmp/uploads/' . basename($file)))->toMediaCollection('files');
         }
@@ -66,15 +72,18 @@ class DirectoryContentController extends Controller
 
         $tags = Tag::all()->pluck('name', 'id');
 
-        $directoryContent->load('ministry', 'tags');
+        $subcategories = DirectorySubCategory::all()->pluck('title', 'id');
 
-        return view('admin.directoryContents.edit', compact('ministries', 'tags', 'directoryContent'));
+        $directoryContent->load('ministry', 'tags', 'subcategories');
+
+        return view('admin.directoryContents.edit', compact('ministries', 'tags', 'subcategories', 'directoryContent'));
     }
 
     public function update(UpdateDirectoryContentRequest $request, DirectoryContent $directoryContent)
     {
         $directoryContent->update($request->all());
         $directoryContent->tags()->sync($request->input('tags', []));
+        $directoryContent->subcategories()->sync($request->input('subcategories', []));
         if (count($directoryContent->files) > 0) {
             foreach ($directoryContent->files as $media) {
                 if (!in_array($media->file_name, $request->input('files', []))) {
@@ -96,7 +105,7 @@ class DirectoryContentController extends Controller
     {
         abort_if(Gate::denies('directory_content_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $directoryContent->load('ministry', 'tags', 'contactServices');
+        $directoryContent->load('ministry', 'tags', 'subcategories', 'contactServices', 'contentdepartmentDirectorySubCategories');
 
         return view('admin.directoryContents.show', compact('directoryContent'));
     }
